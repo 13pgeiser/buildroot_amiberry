31eac0f8f7c2ac5ecf75dc86ecab06adda2c0f0c
Pascal Geiser <pgeiser@pgeiser.com>
2021-12-24 14:03:39 +0100

    Initial commit!

---
+++ package/amiberry/amiberry.mk
@@ -0,0 +1,46 @@
+################################################################################
+#
+# amiberry
+#
+################################################################################
+
+AMIBERRY_VERSION = v4.1.6
+AMIBERRY_SITE = https://github.com/midwan/amiberry.git
+AMIBERRY_SITE_METHOD = git
+AMIBERRY_GIT_SUBMODULES = YES
+AMIBERRY_DEPENDENCIES = sdl2 sdl2_image sdl2_ttf libmpeg2 mpg123 flac
+AMIBERRY_LICENSE = GPL-3.0
+AMIBERRY_LICENSE_FILES = COPYING
+AMIBERRY_PLATFORM=rpi4-64-sdl2
+
+define AMIBERRY_BUILD_CMDS
+	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
+	CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
+	LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
+	$(MAKE) CXX="$(TARGET_CXX)" \
+		CC="$(TARGET_CC)" \
+		LD="$(TARGET_LD)" \
+		AS="$(TARGET_AS)" \
+		STRIP="$(TARGET_STRIP)" \
+		-C $(@D) \
+		-f Makefile \
+		PLATFORM="$(AMIBERRY_PLATFORM)" \
+		SDL_CONFIG="$(STAGING_DIR)/usr/bin/sdl2-config"
+endef
+
+define AMIBERRY_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/amiberry $(TARGET_DIR)/usr/bin/amiberry
+	mkdir -p $(TARGET_DIR)/usr/share/amiberry
+	cp -R $(@D)/data $(TARGET_DIR)/usr/share/amiberry
+	cp -R $(@D)/kickstarts $(TARGET_DIR)/usr/share/amiberry
+	cp -R $(@D)/whdboot $(TARGET_DIR)/usr/share/amiberry
+	rm $(TARGET_DIR)/usr/share/amiberry/whdboot/save-data/Savegames/foo.txt
+	rm $(TARGET_DIR)/usr/share/amiberry/whdboot/save-data/Kickstarts/foo.txt
+	rm $(TARGET_DIR)/usr/share/amiberry/whdboot/save-data/Debugs/foo.txt
+	rm $(TARGET_DIR)/usr/share/amiberry/whdboot/save-data/Autoboots/foo.txt
+	# Copy RTB files
+	mkdir -p $(TARGET_DIR)/home/amiga/Kickstarts && \
+		cp $(@D)/whdboot/save-data/Kickstarts/*.RTB $(TARGET_DIR)/home/amiga/Kickstarts/
+endef
+
+$(eval $(generic-package))
