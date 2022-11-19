9cacfdec4c7214f6dbd77ee986cf001f569f0214
Pascal Geiser <10461883+13pgeiser@users.noreply.github.com>
2022-11-19 12:31:41 +0100

    Move all files in "pag" folder. Add build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ex
+BUILDROOT_VERSION=2021.02.7
+EXT=bz2
+BUILDROOT_VERSION=2021.11
+EXT=xz
+if [ ! -e "buildroot-${BUILDROOT_VERSION}.tar.${EXT}" ]; then
+	wget "https://buildroot.org/downloads/buildroot-${BUILDROOT_VERSION}.tar.${EXT}"
+fi
+if [ ! -e "buildroot-${BUILDROOT_VERSION}" ]; then
+	if [ "$EXT" == "xz" ]; then
+		tar xvJf "buildroot-${BUILDROOT_VERSION}.tar.${EXT}"
+	fi
+	if [ "$EXT" == "bz2" ]; then
+		tar xvjf "buildroot-${BUILDROOT_VERSION}.tar.${EXT}"
+	fi
+fi
+sudo apt install -y sed make binutils gcc g++ bash patch gzip bzip2 perl tar cpio python unzip rsync wget libncurses-dev
+cd "buildroot-${BUILDROOT_VERSION}"
+make BR2_EXTERNAL=../pag raspberrypi4_64_defconfig
+make BR2_EXTERNAL=../pag all 2>&1 | tee make.log
+
