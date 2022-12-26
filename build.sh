9a6e06106b5a2f872e376306e3e20ec955308d3d
Pascal Geiser <10461883+13pgeiser@users.noreply.github.com>
2022-12-26 16:55:43 +0100

    Split build in multiple steps

--- build.sh
+++ build.sh
@@ -2,22 +2,55 @@
 set -ex
 EXT=xz
 BUILDROOT_VERSION=2022.02.1
-if [ ! -e "buildroot-${BUILDROOT_VERSION}.tar.${EXT}" ]; then
-	wget "https://buildroot.org/downloads/buildroot-${BUILDROOT_VERSION}.tar.${EXT}"
+if [ -z "$1" ]; then
+	steps="buildroot apt defconfig sdk build release"
+else
+	steps="$@"
 fi
-if [ ! -e "buildroot-${BUILDROOT_VERSION}" ]; then
-	if [ "$EXT" == "xz" ]; then
-		tar xvJf "buildroot-${BUILDROOT_VERSION}.tar.${EXT}"
-	fi
-	if [ "$EXT" == "bz2" ]; then
-		tar xvjf "buildroot-${BUILDROOT_VERSION}.tar.${EXT}"
-	fi
-fi
-sudo apt install -y sed make binutils gcc g++ bash patch gzip bzip2 perl tar cpio python2 python3 unzip rsync wget libncurses-dev
-(
-	cd "buildroot-${BUILDROOT_VERSION}" || exit 1
-	make BR2_EXTERNAL=../pag raspberrypi4_64_defconfig
-	make BR2_EXTERNAL=../pag all 2>&1 | tee make.log
-)
-mkdir -p release
-cp "buildroot-${BUILDROOT_VERSION}/output/images/sdcard.img" release/
+for step in $steps; do
+	case $step in
+		"buildroot")
+			if [ ! -e "buildroot-${BUILDROOT_VERSION}.tar.${EXT}" ]; then
+				wget "https://buildroot.org/downloads/buildroot-${BUILDROOT_VERSION}.tar.${EXT}"
+			fi
+			if [ ! -e "buildroot-${BUILDROOT_VERSION}" ]; then
+				if [ "$EXT" == "xz" ]; then
+					tar xvJf "buildroot-${BUILDROOT_VERSION}.tar.${EXT}"
+				fi
+				if [ "$EXT" == "bz2" ]; then
+					tar xvjf "buildroot-${BUILDROOT_VERSION}.tar.${EXT}"
+				fi
+			fi
+			;;
+		"apt")
+			sudo apt install -y sed make binutils gcc g++ bash patch gzip bzip2 perl tar cpio python2 python3 unzip rsync wget libncurses-dev
+			;;
+		"defconfig")
+			(
+			cd "buildroot-${BUILDROOT_VERSION}" || exit 1
+			make BR2_EXTERNAL=../pag raspberrypi4_64_defconfig
+			)
+			;;
+		"sdk")
+			(
+			cd "buildroot-${BUILDROOT_VERSION}" || exit 1
+			make BR2_EXTERNAL=../pag prepare-sdk
+			)
+			;;
+		"build")
+			(
+			cd "buildroot-${BUILDROOT_VERSION}" || exit 1
+			make BR2_EXTERNAL=../pag all 2>&1 | tee make.log
+			)
+			;;
+		"release")
+			mkdir -p release
+			cp "buildroot-${BUILDROOT_VERSION}/output/images/sdcard.img" release/
+			;;
+		*)
+			echo "Unsupported step: $step"
+			exit 1
+			;;
+
+	esac
+done
